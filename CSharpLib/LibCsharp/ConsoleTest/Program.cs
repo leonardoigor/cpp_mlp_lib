b126e08b870273090a0845f8d8f8fa54bab2237e
igor <igor_mendonca@outlook.com.br>
2024-05-18 19:36:32 -0300

    charp lib

---
+++ CSharpLib/LibCsharp/ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using ML;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new NNModel();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CSharpLib/LibCsharp/ML/NNModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ML
+{
+    public class NNModel
+    {
+        [DllImport("NeuralNetwork.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Add(int a, int b);
+    }
+}
